9a233b26b7c030e4030f754fa8b7fd860768fc32
RoleOnce <malcolmuzuriaga@hotmail.com>
2024-09-05 14:01:44 +0200

    Added ResponseEntity

--- src/main/java/org/roleonce/projektarbete_web_services/controller/MovieController.java
+++ src/main/java/org/roleonce/projektarbete_web_services/controller/MovieController.java
@@ -1,19 +1,36 @@
 package org.roleonce.projektarbete_web_services.controller;
 
 import org.roleonce.projektarbete_web_services.model.Movie;
+import org.roleonce.projektarbete_web_services.repository.MovieRepository;
 import org.roleonce.projektarbete_web_services.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
-    @Autowired
+    private final MovieRepository movieRepository;
     private ApiService apiService;
 
-    @GetMapping("/movies/{id}")
+    @Autowired
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllUsers() {
+
+        return ResponseEntity.ok(movieRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
     public Movie getMovie(@PathVariable int id) {
         return apiService.getMovieId(id);
     }
