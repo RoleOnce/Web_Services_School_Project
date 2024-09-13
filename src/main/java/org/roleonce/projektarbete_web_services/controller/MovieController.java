c3c43838d44017ff3a8035c67c2fbfb099ea8125
RoleOnce <malcolmuzuriaga@hotmail.com>
2024-09-13 10:38:13 +0200

    Simplifying of the Controller

--- src/main/java/org/roleonce/projektarbete_web_services/controller/MovieController.java
+++ src/main/java/org/roleonce/projektarbete_web_services/controller/MovieController.java
@@ -2,14 +2,12 @@ package org.roleonce.projektarbete_web_services.controller;
 
 import org.roleonce.projektarbete_web_services.model.Movie;
 import org.roleonce.projektarbete_web_services.repository.MovieRepository;
-import org.roleonce.projektarbete_web_services.response.ErrorResponse;
 import org.roleonce.projektarbete_web_services.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -33,69 +31,31 @@ public class MovieController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getMovie(@PathVariable int id) {
 
-        return apiService.getMovieWithId(id);
+        return apiService.getMovieById(id);
     }
 
     @PostMapping("/{id}/review")
     public ResponseEntity<String> postReview(@PathVariable Long id, @RequestBody Movie movie) {
 
-        Optional<Movie> reviewMovie = movieRepository.findById(id);
-
-        if (reviewMovie.isPresent()) {
-            Movie existingMovie = reviewMovie.get();
-            existingMovie.setMovie_review(movie.getMovie_review());
-
-            movieRepository.save(movie);
-
-            return ResponseEntity.ok("REVIEW added to " + id);
-        }
-        return ResponseEntity.notFound().build();
+        return apiService.postAReviewById(id, movie);
     }
 
     @PostMapping("/{id}/save")
-    public ResponseEntity<ErrorResponse> saveMovie(@PathVariable int id) {
+    public ResponseEntity<?> saveMovie(@PathVariable int id) {
 
-        return (ResponseEntity<ErrorResponse>) apiService.saveMovieWithId(id);
+        return apiService.saveMovieById(id);
     }
 
     @PutMapping("/{id}/update")
-    public ResponseEntity<Movie> updateMovieById(@PathVariable Long id, @RequestBody Movie movie) {
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
 
-        Optional<Movie> updateMovie = movieRepository.findById(id);
-
-        if (updateMovie.isPresent()) {
-            Movie existingMovie = updateMovie.get();
-            if (movie.getTitle() != null) {
-                existingMovie.setTitle(movie.getTitle());
-            }
-            if (movie.getOverview() != null) {
-                existingMovie.setOverview(movie.getOverview());
-            }
-            if (movie.getMovie_review() != null) {
-                existingMovie.setMovie_review(movie.getMovie_review());
-            }
-            if (movie.getRelease_date() != null) {
-                existingMovie.setRelease_date(movie.getRelease_date());
-            }
-            if (movie.getVote_average() != null) {
-                existingMovie.setVote_average(movie.getVote_average());
-            }
-
-            movieRepository.save(existingMovie);
-
-            return ResponseEntity.ok(existingMovie);
-        } else {
-            return ResponseEntity.notFound().build(); // Returnera 404 om filmen inte hittas
-        }
+        return apiService.updateMovieCredentialsById(id, movie);
     }
 
     @DeleteMapping("/{id}/delete")
-    public ResponseEntity<String> deleteMovieById(@PathVariable Long id) {
+    public ResponseEntity<String> deleteMovie(@PathVariable Long id) {
 
-        Optional<Movie> movie = movieRepository.findById(id);
-
-        movie.ifPresent(movieRepository::delete);
-        return ResponseEntity.ok("Movie deleted");
+        return apiService.deleteMovieById(id);
     }
 
 }
--- src/main/java/org/roleonce/projektarbete_web_services/service/ApiService.java
+++ src/main/java/org/roleonce/projektarbete_web_services/service/ApiService.java
@@ -6,8 +6,11 @@ import org.roleonce.projektarbete_web_services.response.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
 public class ApiService {
@@ -21,7 +24,7 @@ public class ApiService {
     final String apiUrl = "https://api.themoviedb.org/3/movie/";
     final String apiKey = "270c87c7d9febee8d8c0856291ff4572";
 
-    public ResponseEntity<?> getMovieWithId(int movieId) {
+    public ResponseEntity<?> getMovieById(int movieId) {
         String url = apiUrl + movieId + "?api_key=" + apiKey;
 
         try {
@@ -39,7 +42,7 @@ public class ApiService {
         }
     }
 
-    public ResponseEntity<?> saveMovieWithId(int movieId) {
+    public ResponseEntity<?> saveMovieById(int movieId) {
         String url = apiUrl + movieId + "?api_key=" + apiKey;
 
         try {
@@ -62,6 +65,57 @@ public class ApiService {
                     .status(404)
                     .body(new ErrorResponse("Movie couldn't be saved due to not found"));
         }
+    }
+
+    public ResponseEntity<String> deleteMovieById(Long movieId) {
+
+        Optional<Movie> movie = movieRepository.findById(movieId);
+
+        movie.ifPresent(movieRepository::delete);
+        return ResponseEntity.ok().body("Movie deleted successfully.");
+    }
+
+    public ResponseEntity<Movie> updateMovieCredentialsById(Long movieId, @RequestBody Movie movie) {
+        Optional<Movie> updateMovie = movieRepository.findById(movieId);
+
+        if (updateMovie.isPresent()) {
+            Movie existingMovie = updateMovie.get();
+            if (movie.getTitle() != null) {
+                existingMovie.setTitle(movie.getTitle());
+            }
+            if (movie.getOverview() != null) {
+                existingMovie.setOverview(movie.getOverview());
+            }
+            if (movie.getMovie_review() != null) {
+                existingMovie.setMovie_review(movie.getMovie_review());
+            }
+            if (movie.getRelease_date() != null) {
+                existingMovie.setRelease_date(movie.getRelease_date());
+            }
+            if (movie.getVote_average() != null) {
+                existingMovie.setVote_average(movie.getVote_average());
+            }
+
+            movieRepository.save(existingMovie);
+
+            return ResponseEntity.ok(existingMovie);
+        } else {
+            return ResponseEntity.notFound().build(); // Returnera 404 om filmen inte hittas
+        }
+    }
+
+    public ResponseEntity<String> postAReviewById(Long movieId, @RequestBody Movie movie) {
+        Optional<Movie> reviewMovie = movieRepository.findById(movieId);
+
+        if (reviewMovie.isPresent()) {
+            Movie existingMovie = reviewMovie.get();
+            existingMovie.setMovie_review(movie.getMovie_review());
+
+            movieRepository.save(movie);
+
+            return ResponseEntity.ok("Review added to movie with id: " + movieId);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
